fd25e0c47b7268959a3f8e638803610be4d89420
Simon Noudelman <snoudelman@email.arizona.edu>
2022-02-05 10:55:22 -0500

    Changed winning flow

--- scripts.js
+++ scripts.js
@@ -30,17 +30,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 }, false);
 
-function xmur3(str) {
-    for(var i = 0, h = 1779033703 ^ str.length; i < str.length; i++) {
-        h = Math.imul(h ^ str.charCodeAt(i), 3432918353);
-        h = h << 13 | h >>> 19;
-    } return function() {
-        h = Math.imul(h ^ (h >>> 16), 2246822507);
-        h = Math.imul(h ^ (h >>> 13), 3266489909);
-        return (h ^= h >>> 16) >>> 0;
-    }
-}
-
 function roll() {
     
     if (rolled == false) {
@@ -137,7 +126,7 @@ function verify (result) {
         }
     }
 
-    alert ("You won with a count of " + counter);
+    $('#counter').text("You won with " + counter + " actions!");
 }
 
 function add () {
@@ -172,5 +161,16 @@ function mulberry32(a) {
       t = Math.imul(t ^ t >>> 15, t | 1);
       t ^= t + Math.imul(t ^ t >>> 7, t | 61);
       return ((t ^ t >>> 14) >>> 0) / 4294967296;
+    }
+}
+
+function xmur3(str) {
+    for(var i = 0, h = 1779033703 ^ str.length; i < str.length; i++) {
+        h = Math.imul(h ^ str.charCodeAt(i), 3432918353);
+        h = h << 13 | h >>> 19;
+    } return function() {
+        h = Math.imul(h ^ (h >>> 16), 2246822507);
+        h = Math.imul(h ^ (h >>> 13), 3266489909);
+        return (h ^= h >>> 16) >>> 0;
     }
 }
